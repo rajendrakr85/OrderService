d31d8726f32aaf33eac88bc9e1f612f822988bd5
Rajendra Kumar <kurajendra@altimetrik.com>
2023-10-23 23:01:23 +0530

    jpa changes

--- src/main/java/com/order/controller/OrderController.java
+++ src/main/java/com/order/controller/OrderController.java
@@ -1,11 +1,13 @@
 package com.order.controller;
 
+import com.common.modal.Product;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,23 +22,23 @@ public class OrderController {
 
     @GetMapping("/")
     @CircuitBreaker(name="circuitbreaker",fallbackMethod = "getFallBackOrders")
-    public String getOrders(){
+    public List<Product> getOrders(){
         log.info("OrderService: inside getOrder before called inventory");
-        String inventoryResponse=restTemplate.getForObject("http://localhost:8082/inventory/",String.class);
+        ResponseEntity<Product[]> inventoryResponse=restTemplate.getForEntity("http://localhost:8082/inventory/",Product[].class);
         log.info("OrderService: inside getOrder after called inventory");
-        return inventoryResponse +" from order service";
+        return List.of(inventoryResponse.getBody());
     }
 
     @PostMapping("/create")
-    public String createOrders(){
+    public Product createOrders(@RequestBody Product product){
         log.info("OrderService: inside createOrder before called inventory");
-        String createOrderResponse=restTemplate.postForObject("http://localhost:8082/inventory/create","This order for mobiles",String.class);
+        Product createProdctResponse=restTemplate.postForObject("http://localhost:8082/inventory/create",product,Product.class);
         log.info("OrderService: inside createOrder after called inventory");
-        return createOrderResponse;
+        return createProdctResponse;
     }
 
-    public String getFallBackOrders(Throwable t){
-        return "This is your orders from fall back methods";
+    public List<Product> getFallBackOrders(Throwable t){
+        return List.of(new Product());
     }
 
 }
